d80a59dcc9cd5b9b060eea6298f6c4e73b540127
dhruv <progammerdhruv@gmail.com>
2025-09-16 22:33:11 +0530

    working on data ingestion file

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,7 @@
+# Database ---> data-----> train test split
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+
