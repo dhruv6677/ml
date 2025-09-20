4521e7af104d307840c129995d29fd03b3fc55ce
dhruv <progammerdhruv@gmail.com>
2025-09-21 00:07:59 +0530

    reading data from mysql is sucess

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-# from .src.mlproject.logger import logging
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataINgestion
+from src.mlproject.components.data_ingestion import DataIngetsionConfig
+
 import sys
 if __name__ == "__main__":
     logging.info("The Execution has started")
 
     try:
-        a=1/0
+        # data_ingestion_config = DataIngetsionConfig()
+        data_ingestion = DataINgestion()
+        data_ingestion.inititate_data_ingestion()
+
     except Exception as e:
         logging.info("custom Exception")
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
